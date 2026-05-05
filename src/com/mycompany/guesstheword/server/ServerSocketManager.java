66fcc351b1b2f9468225007599fa890d3812d771
gmellone <g.mellone6@studenti.unisa.it>
2026-05-05 15:20:32 +0200

    Creazione Progetto e Classi Iniziali
    Creazione Package Server con ServerSocketManager e ServerMain, creazione package Client con ClientSocketManager e ClientMain e creazione package common con Message e MessageType

---
+++ src/com/mycompany/guesstheword/server/ServerSocketManager.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.guesstheword.server;
+
+/**
+ *
+ * @author Giovanni
+ */
+public class ServerSocketManager {
+    
+}
